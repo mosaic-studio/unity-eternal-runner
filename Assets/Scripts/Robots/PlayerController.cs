50b51fccef0c232cededf38d0924e75c81592c19
Marlon Baptista de Quadros <marlonbaptista@hotmail.com>
2017-12-09 21:31:02 -0200

    - Envio dos primeiros materiais de teste

---
+++ Assets/Scripts/Robots/PlayerController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+	public float maxSpeed = 5.0f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Move();
+	}
+
+	void Move()
+	{
+		Vector3 velocity = new Vector3(Input.GetAxis("Horizontal") * maxSpeed * Time.deltaTime, 0, 0);
+
+		transform.position += velocity;
+		
+	}
+}
---
+++ Assets/Tilemap/Level00_tileset.tsx
@@ -0,0 +1,31 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="Level00_tileset" tilewidth="70" tileheight="70" tilecount="6" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="70" height="70" source="../Spritesheet/Itens/grassLeft.png"/>
+  <objectgroup draworder="index">
+   <object id="1" x="-1.5" y="0" width="71.5" height="69"/>
+  </objectgroup>
+ </tile>
+ <tile id="1">
+  <image width="70" height="70" source="../Spritesheet/Itens/grassMid.png"/>
+  <objectgroup draworder="index">
+   <object id="1" x="0.5" y="1" width="68.5" height="69"/>
+  </objectgroup>
+ </tile>
+ <tile id="2">
+  <image width="70" height="70" source="../Spritesheet/Itens/grassRight.png"/>
+  <objectgroup draworder="index">
+   <object id="1" x="0.5" y="0.5" width="68.5" height="69"/>
+  </objectgroup>
+ </tile>
+ <tile id="3">
+  <image width="70" height="70" source="../Spritesheet/Itens/signLeft.png"/>
+ </tile>
+ <tile id="4">
+  <image width="70" height="70" source="../Spritesheet/Itens/signRight.png"/>
+ </tile>
+ <tile id="5">
+  <image width="70" height="70" source="../Spritesheet/Itens/grassCenter.png"/>
+ </tile>
+</tileset>
